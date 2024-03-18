4fa39e506f5a3e9d67081ec05c194193f2a9c66b
dleksmd22 <leedn0697@gmail.com>
2024-03-18 16:16:00 +0900

    240318

---
+++ 20240318/WPFclnt/MainWindow.xaml.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+// OpenCV 사용을 위한 using
+using OpenCvSharp;
+using OpenCvSharp.WpfExtensions;
+
+// Timer 사용을 위한 using
+using System.Windows.Threading;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace WPF
+{
+
+    // OpenCvSharp 설치 시 Window를 명시적으로 사용해 주어야 함 (window -> System.Windows.Window)
+    public partial class MainWindow : System.Windows.Window
+    {
+        // 필요한 변수 선언
+        VideoCapture cam;
+        Mat frame;
+        DispatcherTimer timer;
+        bool is_initCam, is_initTimer;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void windows_loaded(object sender, RoutedEventArgs e)
+        {
+            // 카메라, 타이머(0.01ms 간격) 초기화
+            is_initCam = init_camera();
+            is_initTimer = init_Timer(0.01);
+
+            // 초기화 완료면 타이머 실행
+            if (is_initTimer && is_initCam) timer.Start();
+        }
+
+        private bool init_Timer(double interval_ms)
+        {
+            try
+            {
+                timer = new DispatcherTimer();
+
+                timer.Interval = TimeSpan.FromMilliseconds(interval_ms);
+                timer.Tick += new EventHandler(timer_tick);
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private bool init_camera()
+        {
+            try
+            {
+                // 0번 카메라로 VideoCapture 생성 (카메라가 없으면 안됨)
+                cam = new VideoCapture(0);
+                cam.FrameHeight = (int)Cam_1.Height;
+                cam.FrameWidth = (int)Cam_1.Width;
+
+                // 카메라 영상을 담을 Mat 변수 생성
+                frame = new Mat();
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+
+
+        private void timer_tick(object sender, EventArgs e)
+        {
+            // 0번 장비로 생성된 VideoCapture 객체에서 frame을 읽어옴
+            cam.Read(frame);
+            // 읽어온 Mat 데이터를 Bitmap 데이터로 변경 후 컨트롤에 그려줌
+            Cam_1.Source = OpenCvSharp.WpfExtensions.WriteableBitmapConverter.ToWriteableBitmap(frame);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // [*] 영상을 사진으로찍음. 
+            Cv2.ImShow("클라?", frame);
+            var test_image = frame;
+            // frame을 저장해야함.
+            Cv2.ImWrite("C:\\test\\TTTTTTTTTTTTTTTTTTTT.jpg", test_image); // 프레임 스크린샷 저장경로+파일명
+            int i = 0;
+            // 업로드 할 파일 정보
+            FileInfo file = new FileInfo("C:\\test\\TTTTTTTTTTTTTTTTTTTT.jpg"); // 작업할 파일 불러오기.
+            //FileInfo file = new FileInfo();
+            //stream을 취득?
+            using (FileStream stream = new FileStream(file.FullName, FileMode.Open, FileAccess.Read))
+            {
+                // 파일 binary를 연다.
+                byte[] data = new byte[file.Length];
+                stream.Read(data, 0, data.Length);
+                // 소켓을 연다
+                using (Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP))
+                {
+                    // 파일 서버로 접속한다. 주소, 포트번호 변경요망
+                    socket.Connect(IPAddress.Parse("127.0.0.1"), 9090);
+                    // 전송 람다 함수 (C++와 약속한 규약대로 데이터를 송수신한다.
+                    Action<byte[]> Send = (b) =>
+                    {
+                        // 먼저 데이터 사이즈를 보냄
+                        socket.Send(BitConverter.GetBytes(b.Length), 4, SocketFlags.None);
+                        // 데이터를 보낸다.
+                        socket.Send(b, b.Length, SocketFlags.None);
+                    };
+                    // 먼저 파일명을 전송? 한? 다? 영상보낼건데?  일단 파일송수신테스트
+                    // unicode가 아닌 utf8형식으로 전송한다.
+                    Send(Encoding.UTF8.GetBytes("Download.jpg\0")); // 서버에서 받은 이미지
+                    // 파일 바이너리 데이터를 보낸다.
+                    Send(data);
+                    // 서버로 부터 byte=1 데이터가 오면 클라를 종료한다.
+                    byte[] ret = new byte[1];
+                    socket.Receive(ret, 1, SocketFlags.None);
+                    if (ret[0] == 1)
+                    {
+                        Console.WriteLine("Completed");
+                    }
+                }
+            }
+
+
+        }
+    }
+}
---
+++ 20240318/dataserv/dataserv.cpp
@@ -0,0 +1,127 @@
+﻿#pragma comment(lib, "ws2_32") // socket 사용을위함
+#pragma warning(disable:4996) // inet_ntoa를 위함
+
+#include <stdio.h> 
+#include <iostream>
+#include <vector>
+#include <thread>
+#include <WinSock2.h> // 소켓쓰기위한 라이브러리
+
+#define BUFFERSIZE 1024 // 수신용버퍼사이즈
+
+using namespace std;
+
+// [1] 클라 데이터 수신하는 함수 ( 사이즈 -> 데이터 순서 수신)
+unsigned char* receive(SOCKET clientSock, int* size)
+{
+	// char* 4바이트로받으면 int형이됨.
+	if (recv(clientSock, (char*)size, 4, 0) == SOCKET_ERROR)
+	{
+		cout << "error" << endl;
+		return nullptr;
+	}
+	// 데이터를 unsigned char 형식으로 받기 = byte
+	unsigned char* buffer = new unsigned char[*size];
+	if (recv(clientSock, (char*)buffer, *size, 0) == SOCKET_ERROR)
+	{
+		cout << "error" << endl;
+		return nullptr;
+	}
+
+	// 받은데이터 리턴하기.
+	return buffer;
+}
+
+// [2] 클라 접속용 쓰레드
+void client(SOCKET clientSock, SOCKADDR_IN clientAddr, vector<thread*>* clientlist)
+{
+	// 클라 접속정보출력
+	cout << "Client connected IP address = " << inet_ntoa(clientAddr.sin_addr) << ":" << ntohs(clientAddr.sin_port) << endl;
+	int size; // 데이터사이즈
+	wchar_t buffer[BUFFERSIZE] = L"C:\\test\\"; // 저장할 경로
+	wchar_t filename[BUFFERSIZE]; // 저장할 파일명변수?
+	unsigned char* data = receive(clientSock, &size); // 데이터받기
+	//1번째 = 파일명, C# -> utf8 (o) 보냄
+	// c++ = unicode로 변환
+	MultiByteToWideChar(CP_UTF8, 0, (const char*)data, size, filename, BUFFERSIZE);
+	delete data; // 수신데이터 메모리에서 삭제
+	wcscat(buffer, filename); // 디렉토리 + 파일명
+	data = receive(clientSock, &size); // 데이터 다시 받기 ^ 업로드하는 파일 데이터이다.
+	FILE* fp = _wfopen(buffer, L"wb"); // 저장할 파일 객체를 받는다.
+	if (fp != NULL)
+	{
+		fwrite(data, 1, size, fp); // 파일저장
+		fclose(fp); // 파일닫기
+	}
+	else
+	{
+		// 에러시 출력
+		cout << "File open failed" << endl;
+	}
+	delete data; // 수신데이터 메모리삭제
+	char ret[1] = { 1 }; // 송신 데이터 선언 바이트 = 1 보내면 송신완료.
+	send(clientSock, ret, 1, 0); // 클라에게 오나료 패킷을 보낸다.
+	closesocket(clientSock); // 소켓 닫는다.
+	// 접속 종료 출력
+	cout << "Client disconnected IP address = " << inet_ntoa(clientAddr.sin_addr) << ":" << ntohs(clientAddr.sin_port) << endl;
+	//쓰레드제거
+	for (auto ptr = clientlist->begin(); ptr < clientlist->end(); ptr++)
+	{
+		if ((*ptr)->get_id() == this_thread::get_id())
+		{
+			clientlist->erase(ptr);
+			break;
+		}
+	}
+}
+
+// 실행함수
+int main()
+{
+	vector<thread*> clientlist; // 클라에 접속중인 client list
+	WSADATA wsaData; // 소켓정보 데이터 설정
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) // 소켓실행
+	{
+		return 1;
+	}
+	SOCKET serverSock = socket(PF_INET, SOCK_STREAM, 0); // internet Stream방식으로 소켓 생성
+	SOCKADDR_IN addr; // 소켓 주소 설정
+
+	memset(&addr, 0, sizeof(addr)); // 구조체 초기화
+	addr.sin_family = AF_INET; 
+	addr.sin_addr.s_addr = htonl(INADDR_ANY); // 로컬설정, 다른컴퓨터로하면 htonl()괄호안의 수정요망
+	addr.sin_port = htons(9090); // 포트
+
+	if (bind(serverSock, (SOCKADDR*)&addr, sizeof(SOCKADDR_IN)) == SOCKET_ERROR) // [2]bind
+	{
+		cout << "error" << endl;
+		return 1;
+	}
+	if (listen(serverSock, SOMAXCONN) == SOCKET_ERROR) // 소켓대기
+	{
+		cout << "error" << endl;
+		return 1;
+	}
+	cout << "Server Start" << endl; // 서버시작
+
+
+	while (1) // 다중접속을 위함
+	{
+		int len = sizeof(SOCKADDR_IN); // 접속설정 구조체 사이즈
+		SOCKADDR_IN clientAddr; // 접속설정 구조체
+		SOCKET clientSock = accept(serverSock, (SOCKADDR*)&clientAddr, &len); // clnt가 접속하면 socket받기
+		clientlist.push_back(new thread(client, clientSock, clientAddr, &clientlist)); // 쓰레드실행, 쓰레드 리스트에 넣기
+	}
+
+	// 종료시 쓰레드 리스트에 남아 있는 쓰레드를 종료할 때까지 기다림
+	if (clientlist.size() > 0)
+	{
+		for (auto ptr = clientlist.begin(); ptr < clientlist.end(); ptr++)
+		{
+			(*ptr)->join();
+		}
+	}
+	closesocket(serverSock); // 소켓종료
+	WSACleanup(); // 소켓 종료
+	return 0;
+}
